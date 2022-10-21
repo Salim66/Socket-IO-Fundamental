e52bf085251b9a82154dc06a66bc9957b9cd0e68
Salim66 <salimhasanriad8@gmail.com>
2022-10-21 19:00:48 +0600

    Express servet convert to Socket IO Server

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const colors = require('colors');
 const path = require('path');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
 
 // init express
 const app = express();
+
+// create express server to raw server
+const httpServer = createServer(app);
+
+// socket server init
+const io = new Server(httpServer); 
 
 
 // route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client.html'));
 });
+
 
 // listen server
 app.listen(5050, () => {
